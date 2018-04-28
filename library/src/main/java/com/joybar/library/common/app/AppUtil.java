5c6cd45a18fea8cda1a4143f989ff6350d37c0de
joybar <myjoybar@gmail.com>
2018-04-28 18:39:34 +0800

    add app debug utils

--- library/src/main/java/com/joybar/library/common/app/AppUtil.java
+++ library/src/main/java/com/joybar/library/common/app/AppUtil.java
@@ -1,9 +1,33 @@
 package com.joybar.library.common.app;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+
 /**
  * Created by joybar on 2018/1/9.
  */
 
 public class AppUtil {
+	private static Boolean isDebug = null;
 
+	/**
+	 * form Trinea
+	 * http://www.trinea.cn/android/android-whether-debug-mode-why-buildconfig-debug-always-false/
+	 * @return
+	 */
+	public static boolean isDebug() {
+		return isDebug == null ? false : isDebug.booleanValue();
+	}
+
+	/**
+	 * Sync lib debug with app's debug value. Should be called in module Application
+	 *
+	 * @param context
+	 */
+	public static void syncIsDebug(Context context) {
+		if (isDebug == null) {
+			isDebug = context.getApplicationInfo() != null &&
+					(context.getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+		}
+	}
 }
